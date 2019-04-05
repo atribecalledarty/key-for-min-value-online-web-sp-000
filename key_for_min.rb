30e15d4d4fcf89b1d3e53c2ff08a67b64b8e142d
Arthur Song <arth3rs0ng@gmail.com>
2019-04-05 05:29:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  comp = 10000
+  key = ""
+  name_hash.each do |key, value|
+    if value < comp
+      comp = value
+    end
+  end
+  if comp = 10000
+    nil
+  else
+    comp
+    
 end
